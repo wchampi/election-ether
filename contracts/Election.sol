1d139a29bb8e7909c99fd200cb4261a244f0448a
Wilson Champi <wchampi86@gmail.com>
2021-04-14 07:28:33 -0500

    add multi candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,10 +1,23 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
-    string public candidate;
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
 
     // Constructor
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
