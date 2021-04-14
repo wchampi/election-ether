d61bf130525b91f8ca87bf66524c80dcb44d9127
Wilson Champi <wchampi86@gmail.com>
2021-04-14 06:17:48 -0500

    first contract

---
+++ contracts/Election.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election {
+    string public candidate;
+
+    // Constructor
+    constructor() public {
+        candidate = "Candidate 1";
+    }
+}
